ab391459ca488c32bee313710c366385a1afa957
Vũ Huy Hoàng <uzumakivuhuyhoang@gmail.com>
2025-08-11 21:24:00 +0700

    Update gemini-model.ts

--- utils/gemini-model.ts
+++ utils/gemini-model.ts
@@ -1,5 +1,5 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const GEMINI_API_KEY = 'AIzaSyDQtlgBfYnwvUhKrs9glNfQWxlPGVLbhdo';
+const GEMINI_API_KEY = '';
 const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
 export const geminiModel = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
